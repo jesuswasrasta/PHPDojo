cd379a3d4751fc5e974af9996d7ead9c7844ee4a
Ferdinando Santacroce <santacroce@intre.it>
2019-03-15 18:48:34 +0100

    JBrains Trivia kata first failing test

---
+++ Trivia/tests/GameTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Test\TripServiceKata\Trip;
+
+use PHPUnit\Framework\TestCase;
+
+
+class GameTest extends TestCase
+{
+    /** @test */
+    public function
+    it_does_something()
+    {
+        $this->fail('This test has not been implemented yet.');
+    }
+}
