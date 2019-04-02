41d42ef895f607788df3b12864a00c4530fec658
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2019-04-02 17:07:55 +0200

    FizzBuzz Kata: test for number 3

--- FizzBuzz/src/FizzBuzz.php
+++ FizzBuzz/src/FizzBuzz.php
@@ -14,7 +14,20 @@ class FizzBuzz
     {
     }
 
-    public function say(int $givenNumber){
-        return $givenNumber;
+
+    /**
+     * @param int $givenNumber
+     * @return string
+     */
+    public function say(int $givenNumber): string
+    {
+        if ($givenNumber == '3')
+        {
+            return "Fizz";
+        }
+        else
+        {
+            return $givenNumber;
+        }
     }
 }
--- FizzBuzz/tests/FizzBuzzTest.php
+++ FizzBuzz/tests/FizzBuzzTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Test\FizzBuzzKata;
 
@@ -8,19 +8,31 @@ use FizzBuzzKata\FizzBuzz;
 class FizzBuzzTest extends TestCase
 {
     /** @test */
-    public function givingNumber1tShouldReturn1() : void {
+    public function givingNumber1tShouldReturn1(): void
+    {
         $fizzbuzz = new FizzBuzz();
-        $expected = 1;
+        $expected = "1";
         $actual = $fizzbuzz->say(1);
 
         $this->assertEquals($expected, $actual);
     }
 
     /** @test */
-    public function givingNumber2tShouldReturn2() : void {
+    public function givingNumber2tShouldReturn2(): void
+    {
         $fizzbuzz = new FizzBuzz();
-        $expected = 2;
+        $expected = "2";
         $actual = $fizzbuzz->say(2);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /** @test */
+    public function givingNumber3tShouldReturnFizz(): void
+    {
+        $fizzbuzz = new FizzBuzz();
+        $expected = "Fizz";
+        $actual = $fizzbuzz->say(3);
 
         $this->assertEquals($expected, $actual);
     }
